ec37a2ff46e92a5f74176aac63afeb3bf2807266
ErikRepin <erik.repin@gmail.com>
2022-10-12 18:23:41 +0300

    Homework

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 
 
+function App() {
+   const radios = [
+    { name: 'High price', value: '1' },
+    { name: 'Low price', value: '2' },
+    
+  ];
+
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
-
+ 
 );
 
 
