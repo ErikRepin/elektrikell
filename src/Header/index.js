603a80ab26c5e59b981413e82c716a6f19090765
ErikRepin <erik.repin@gmail.com>
2022-10-12 20:45:52 +0300

    new footer & countdown package

--- src/Header/index.js
+++ src/Header/index.js
@@ -6,15 +6,20 @@ import ToggleButton from 'react-bootstrap/ToggleButton';
 function Header(props) {
 
     const radios = [
-        { name: 'High price', value: '1' },
-        { name: 'Low Price', value: '2' },
+        { name: 'High price', value: 'low' },
+        { name: 'Low Price', value: 'high' },
     ];
+    function handleOnChange(event) {
+      console.log(event);
+      props.setRadioValue(event.currentTarget.value);
+    }
 
     return (
         <>
             <Row>
                 <Col><h3>Elektrikell</h3></Col>
             </Row>
+
             <Row>
                 <Col>Status</Col>
                 <Col>
@@ -24,11 +29,11 @@ function Header(props) {
                                 key={idx}
                                 id={`radio-${idx}`}
                                 type="radio"
-                                variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                                variant={idx % 2 ? 'outline-danger' : 'outline-success'}
                                 name="radio"
                                 value={radio.value}
                                 checked={props.radioValue === radio.value}
-                                onChange={(e) => props.setRadioValue(e.currentTarget.value)}
+                                onChange={handleOnChange}
                             >
                                 {radio.name}
                             </ToggleButton>
