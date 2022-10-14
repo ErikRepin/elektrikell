88ca9b0d64d6d4d80590a35c4ce7a586e5848a32
ErikRepin <erik.repin@gmail.com>
2022-10-14 20:32:36 +0300

    description main

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// ReactDom - otve4aet za svjaz' mezdu reactom i DOM;
+// DOM - (Document object model) - Eto struktura html document v javascript objekte.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Zdes' my berjom iz document(DOM) element s id "root" i vstavljaem v etot element react prilozenie."
 root.render(
     <App />
  
 );
-
-
+// render berjot React elementy/ component i obrabatqivaet iv v html(DOM)
+//Prostoe objasnenie po4emu reat:
+// iz-za componentov i sostojanija react izmenenija tol'ko to chto neobhodimo, sledstvenno rabota saita bqstraja.
