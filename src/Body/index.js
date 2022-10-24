4df04ab54a8a73ce8f9fad96cf626afb94a50f07
ErikRepin <erik.repin@gmail.com>
2022-10-24 20:59:50 +0300

    logic description

--- src/Body/index.js
+++ src/Body/index.js
@@ -23,31 +23,49 @@ function Body({
     const [x2, setX2] = useState(0);
 
     useEffect(() => {
+      // vnutri otkrqli asynhronnuju funkciju kotoraja moentalno i zapuskaetsja 
+      // nemedlenno vqzqvaemaja funkcija
         (async function () {
+          // try - probuet vqpolnit vsjo 4to v jego scope. {}
+          // pri obnuruzenii oshibki try ostanavlivaetsja i zapuskaet cacth scope {} peredav emu oshibku
             try {
+               // mq sprashivaem est li otvet s api v sostojanii komponenta
                 if (!response) { 
+                  // esli netu to mi delaem zapros i sohranjaem v sostjoanii komponenta
                     const response = await getPriceData();
-                    setRsponse(response.data);
-                    return;
+                    setResponse(response.data);
+                    return; // garantiruem 4tobq kod dalshe ne vqpolnalsja
                 }
-
+                   // vzjav otvet s api my vqbiraem vqbrannqi pol'zovatelem stranu (ee) 
+                   // na massive s dannqimi mq zapuskaem cicl map kotorqi nam vozvrashet novqi massive
                    let priceData = priceData[selectedCountry.key].map(dataObject => {
+                    // v cicle mq s timestamp vzjali chasq "HH" i naznachili x i y
+                    // y = cena
+                    // x = vremja
+                    // timestamp 
                         return {
                             x: moment.unix(dataObject.timestamp).format('HH'),
                             y: dataObject.price,
                             timestamp: dataObject.timestamp,
                         };
                     });
-                    setData(priceData);
+                    setData(priceData); // naznachili novqi massive s obrabotannqmi dannqmi 
                 }
               
+                // ishem index v kotorom zapisan dannqi 4as
                 const hourNowI = priceData.findIndex(dataObject => {
                     return dataObject.x === moment().format('HH');
                 });
                 setHourNowI(hourNowI);
 
+                // vqdeljaem/filtruem massive gde toko budushie vremja prskolko znaem 4to budushee vremja nastupaet 
                 const futureData = priceData.filter((v, i) => i >= 9);
                 const areaPrices = [];
+                // dopustim ishem 3 samqh deshovqi chasov
+                // zapuskaem cikl na budushie vremena
+                // kazdqi cikl berjot 3 4asa / 3 elemta s massiva 
+                // sumiruet ih i zapisqvaet v novqi massive s tekushem indexom
+                // takim orazom mq nahodim samqi deshovqi diapazon v 3 4asa
                 futureData.forEach((v, i, arr) => {
                     const partData = arr.slice(i, i + hourValue + 1);
                     if (partData.length === hourValue + 1) {
@@ -58,10 +76,12 @@ function Body({
                     return;
 
                 });
+                // sortiruem po summe, deshovqi v nachalo
                 areaPrices.sort((a, b) => a.result - b.result);
                 if (radioValue === 'low') {
-                    
-
+                  // esli hotim znat' deshovqe cenq
+                  // berjom vremena i ishem objeckt s cenoi po pervomu/deshovomu indexu
+                  // sostavljaem nashq dannqe dlja grafika i shot4ika
                     setBestTimeRange({
                         from: futureData[areaPrices[0].i].x,
                         until: futureData[areaPrices[0].i + hourValue].x,
@@ -69,6 +89,7 @@ function Body({
                         bestPrice: futureData[areaPrices[0].i].y,
                     });
                 } else {
+                  // esli hotim samoe dorogoe to mq perevorachivaem same deshovqe summq. Teper porjadok s dorogogo k deshovqm
                     areaPrices.reverse();
                     setWorstTimeRange({
                         from: futureData[areaPrices[0].i].x,
@@ -77,16 +98,17 @@ function Body({
                     });
 
                 }
-
+                // dobavljaem proshloe dlja grafika n naznashem v tochk
                 setX1(9 + areaPrices[0].i);
                 const x2 = 9 + areaPrices[0].i + hourValue;
                 setX2(x2);
+
               } catch (error) {
                 setShowError(true);
                 setErrorMessage(error.message);
             }
         })();
-    } [hourValue, data, setBestTimeRange, setWorstTimeRange, radioValue, selectedCountry, response];
+    }, [hourValue, data, setBestTimeRange, setWorstTimeRange, radioValue, selectedCountry, response]);
 
 return (
         <>
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,12 @@
-import moment from 'moment';
+import moment from 'moment'; // npm paket lja rabotq s datoj/4islo i vremja
 
- const apiUrl = "https://dashboard.elering.ee/api";
+ const apiUrl = "https://dashboard.elering.ee/api"; // api url
 
+ // exportiruem asinhronnuju funkciju, kotoraja zaprashivaet tekushiju stoimost' elektro energiju
+ // zapros GET /nps/price/ee/current - end point 
+ // await pozvoljaet dozidatsja otveta s api
+ // fetch vozvrashaet Promise/obehsanije i pri pravel'nom vqpolnenii objekt response
+ // Kazdqi response imjet fukciju .jason() kotoraja perevodit JSON v ja objekt
  export async function getCurrentPrice() {
       const country = 'EE';
       const response = await fetch(`${apiUrl}/nps/price/${country}/current`);
@@ -9,8 +14,13 @@ import moment from 'moment';
  };
 
 export async function getPriceDate() {
+   // moment() - vqdajot mement object s tekushem vremeni i datoj
+   // .utc() - konvertiruet eto v nulevoi 4asovoi pojas
+   // substract - vqchetaet
+   // .format() prevrashaet moment object v string s udobnqm formatom chtenija 
    const start = moment().utc().subtract(10, 'hours').format();
    const end = moment().utc().add(30, 'hours').format();
+   // URLSearchParams - prevrashaet js object v strochku dlja url
    const params = new URLSearchParams({start, end});
    const response = await fetch(`${apiUrl}/nps/price?${params}`);
    return response.json();
