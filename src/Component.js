5d87dc3bcabbc9a2085df580719ae266468e0764
ErikRepin <erik.repin@gmail.com>
2022-10-31 18:57:59 +0200

    SourceControlCleenUp

--- src/Component.js
+++ src/Component.js
@@ -1,27 +1,27 @@
 import React, {useState} from 'react';
 
-//function FComponent(){     // komponenetq objavljajutsja s bolshoi bukva
-//   return (
-//     <div>Privet ot FComponent</div>
-//  )
-//}
+// function FComponent(){
+//     return (
+//         <div>Privet ot FComponent</div>
+//     )
+// }
 
-const SComponent = (props) => { // komponenetq objavljajutsja s bolshoi bukva // Strelo4naja func
-   const [count, setCount] = useState(0);
-   return (
-      <div>
-         Privet ot {props.firstName} {props.lastName} {count}
-         <button onClick={() => setCount(count + 1)}>+</button>
-         <button onClick={() => setCount(0)}>Null</button>
-         </div>
-   )
+const SComponent = (props) => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            Privet ot {props.firstName} {props.lastName} {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(0)}>Null</button>
+        </div>
+    )
 }
 
-//class CComponent extends React.Component {
-//   render() {
-//      return (
-//        <div>Privet ot Class Component</div>
-//      )
-//  }
-//}
+// class CComponent extends React.Component {
+//     render() {
+//         return (
+//             <div>Privet ot Class Componet</div>
+//         )
+//     }
+// }
 export default SComponent;
--- src/Footer/high.js
+++ src/Footer/high.js
@@ -1,27 +1,28 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useSelector } from `react-redux`;
+import { useSelector } from 'react-redux';
 
-function High({worstTimeRange}) { 
+function High() {
     const currentPrice = useSelector((state) => state.currentPrice);
-
-   return (
-      <>
-          <Row>
-              <Col>Järgmine tiputund on</Col>
-          </Row>
-          <Row>
-              <Col>{`${worstTimeRange.from}:00st ${worstTimeRange.until}:00no`}</Col>
-          </Row>
-          <Row>
-              <Col>Siis on kilovatt-tunni hind 30.00 senti, mis on 12% kallim kui praegu</Col>
-          </Row>
-          <Row>
-              <Col>Soovitame tiptundide ajal vähendada elektri tarbimist, et aidata kaasa Euroopa ühisele eesmärgile alandada tiputundidel -5% elektri tarbmist ja vähendada maagaasi nõudlust. Loe lähemalt</Col>
-          </Row>
-      </>
-  );
-
+    const worstTimeRange = useSelector((state) => state.worstTimeRange);
+    return (
+        <div className="text-center">
+            <Row>
+                <Col>Järgmine tiputund on</Col>
+            </Row>
+            <Row>
+                <Col>{`${worstTimeRange.from}:00st ${worstTimeRange.until}:00ni`}</Col>
+            </Row>
+            <Row>
+                <Col>Siis on kilovatt-tunni hind {currentPrice} eur, 
+                    mis on {Math.round(100 - worstTimeRange.worstPrice / currentPrice * 100)}% kallim kui praegu
+                </Col>
+            </Row>
+            <Row>
+                <Col>Soovitame tiptundide ajal vähendada elektri tarbimist, et aidata kaasa Euroopa ühisele eesmärgile alandada tiputundidel -5% elektri tarbmist ja vähendada maagaasi nõudlust. Loe lähemalt</Col>
+            </Row>
+        </div>
+    );
 }
 
 export default High;
