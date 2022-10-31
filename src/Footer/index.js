f969540f6cb9dd22e56c90c1db8caebdfca785f3
ErikRepin <erik.repin@gmail.com>
2022-10-31 20:10:11 +0200

    redux & router description

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,19 +1,24 @@
 import High from './high';
 import Low from './low';
-import {}
+import './footer.scss';
+import { Route, Routes } from 'react-router-dom';
+// biblioteka kotoraja nam pomogaet pokazqvat te komponenntq kotorqe bqli zaproshennq 4erez url
 
 function Footer(props) {
-  const radioValue = useSelector((state) => state.radioValue);
-
+    // Routes eto wrapper nashqh marshrutov
+    // Route eto marshrut v kotorqi peredajom komponent 4erez props
+    // path sovpadaet s nashim url i Route renderit ego polu4ennqi komponent
+    // v path 4erez : my mozem peredat komponent parametra url
     return (
-    
-      <div id="footer">
-        {props.radioValue === 'low' ? (<Low {...props} />) : 
-        (<High />)}
-        (<High 
+        <div id="footer">
+            <Routes>
+                <Route path="/" element={<Low {...props} />} />
+                <Route path="/low" element={<Low {...props} />} />
+                <Route path="/low/:hours" element={<Low {...props} />} />
+                <Route path="/high" element={<High />} />
+            </Routes>
         </div>
-      </>
-    );  
+    );
 }
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { store } from './services/stateService';
+import { BrowserRouter } from 'react-router-dom';
+// Provider eto vspomogatelnqi komponent dlja rabotq s hranilishem redux
+//
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// BrowserRouter - eto glavnqi komponent react routera kotorqi govorit, 4to zdes ispolzuetsja 
+// marshrutq/Routes. marshrutq idut s adressa/url
 root.render(
-    <App />
- 
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
-
-
